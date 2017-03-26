d730d211310855d6bca4e599abe5aeef18955948
Jack_lin <401788217@qq.com>
2017-03-26 22:06:13 +0800

    optime <NSDictionary+JLUtils> methods

--- sources/NSDictionary+JLUtils.h
+++ sources/NSDictionary+JLUtils.h
@@ -2,7 +2,7 @@
 //  NSDictionary+JLUtils.h
 //  JLUtils<https://github.com/123sunxiaolin/JLUtils>
 //
-//  Created by Sunxiaolin on 17/3/26.
+//  Created by Jacklin on 17/3/26.
 //  Copyright © 2017年 com.jack.lin. All rights reserved.
 //
 //  This source code is licensed under the MIT-style license found in the
@@ -23,5 +23,93 @@
  *  @return json数据
  */
 - (NSData *)JL_jsonData;
-
+/**
+ *  添加key对应的对象为空的处理
+ *
+ *  @param key 键
+ *
+ *  @return 对象
+ */
+- (id)JL_objectOrNilForKey:(id)key;
+/**
+ *  返回字符串类型的对象
+ *
+ *  @param key 键
+ *
+ *  @return 对应的字符串
+ */
+- (NSString *)JL_stringForKey:(id)key;
+/**
+ *  返回不为空的字符串对象
+ *
+ *  @param key 键
+ *
+ *  @return 字符串
+ */
+- (NSString *)JL_stringOrEmptyStringForKey:(id)key;
+/**
+ *  返回字典类型的对象
+ *
+ *  @param key 键
+ *
+ *  @return 字典
+ */
+- (NSDictionary *)JL_dictionaryForKey:(id)key;
+/**
+ *  返回数组类型的对象
+ *
+ *  @param key 键
+ *
+ *  @return 数组
+ */
+- (NSArray *)JL_arrayForKey:(id)key;
+/**
+ *  返回NSNumber类型的对象
+ *
+ *  @param key 键
+ *
+ *  @return NSNumber
+ */
+- (NSNumber *)JL_numberForKey:(id)key;
+/**
+ *  返回NSURL类型的对象
+ *
+ *  @param key 键
+ *
+ *  @return NSURL
+ */
+- (NSURL *)JL_urlForKey:(id)key;
+/**
+ *  返回bool类型
+ *
+ *  @param key 键
+ *
+ *  @return bool
+ */
+- (BOOL)JL_boolForKey:(id)key;
+/**
+ *  返回 ISO8601 格式字符串对应的日期
+ *
+ *  @param key 键
+ *
+ *  @return 日期对象
+ */
+- (NSDate *)JL_dateForKey:(id)key;
+/**
+ *  返回指定秒数对应的日期(since 1970)
+ *
+ *  @param key 键
+ *
+ *  @return 对应的日期
+ */
+- (NSDate *)JL_dateForKeySince1970:(id)key;
+/**
+ *  返回指定日期格式的NSDate对象
+ *
+ *  @param key          键
+ *  @param formatString 日期格式
+ *
+ *  @return NSDate对象
+ */
+- (NSDate *)JL_dateForKey:(id)key withFormat:(NSString *)formatString;
 @end
--- sources/NSDictionary+JLUtils.m
+++ sources/NSDictionary+JLUtils.m
@@ -2,7 +2,7 @@
 //  NSDictionary+JLUtils.h
 //  JLUtils<https://github.com/123sunxiaolin/JLUtils>
 //
-//  Created by Sunxiaolin on 17/3/26.
+//  Created by Jacklin on 17/3/26.
 //  Copyright © 2017年 com.jack.lin. All rights reserved.
 //
 //  This source code is licensed under the MIT-style license found in the
@@ -40,6 +40,90 @@
         
     }
     return jsonData;
+}
+
+- (id)JL_objectOrNilForKey:(id)key{
+    id object = [self objectForKey:key];
+    if (object == [NSNull null]) {
+        return nil;
+    }
+    return object;
+}
+
+- (NSString *)JL_stringForKey:(id)key{
+    NSString *string = [self JL_objectOrNilForKey:key];
+    if ([string isKindOfClass:[NSString class]]  && (string.length > 0)) {
+        return string;
+    }
+    return nil;
+}
+
+- (NSString *)JL_stringOrEmptyStringForKey:(id)key{
+    NSString *string = [self JL_stringForKey:key];
+    if (!string) return @"";
+    return string;
+}
+
+- (NSDictionary *)JL_dictionaryForKey:(id)key{
+    NSDictionary *dictionary = [self JL_objectOrNilForKey:key];
+    if ([dictionary isKindOfClass:[NSDictionary class]] && [dictionary allKeys].count > 0) {
+        return dictionary;
+    }
+    return nil;
+}
+
+- (NSArray *)JL_arrayForKey:(id)key{
+    NSArray *array = [self JL_objectOrNilForKey:key];
+    if ([array isKindOfClass:[NSArray class]] && array.count > 0) {
+        return array;
+    }
+    return nil;
+}
+
+- (NSNumber *)JL_numberForKey:(id)key{
+    NSNumber *number = [self JL_objectOrNilForKey:key];
+    if ([number isKindOfClass:[NSNumber class]]) {
+        return number;
+    }
+    return nil;
+}
+
+- (NSURL *)JL_urlForKey:(id)key{
+    return [NSURL URLWithString:[self JL_stringForKey:key]];
+}
+
+- (BOOL)JL_boolForKey:(id)key{
+    return [[self JL_stringForKey:key] boolValue];
+}
+
+- (NSDate *)JL_dateForKey:(id)key{
+    NSString *string = [self JL_stringForKey:key];
+    if (!string) return nil ;
+    
+    if ([string hasPrefix:@"Z"]) {
+        string = [[string substringToIndex:string.length - 1] stringByAppendingString:@"-0000"];
+    }
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZ"];
+    NSDate *date = [dateFormatter dateFromString:string];
+    return date;
+}
+
+- (NSDate *)JL_dateForKeySince1970:(id)key{
+    NSNumber *number = [self JL_numberForKey:key];
+    if (!number) return nil;
+    NSTimeInterval timeInterval = [number doubleValue];
+    return [NSDate dateWithTimeIntervalSince1970:timeInterval];
+}
+
+- (NSDate *)JL_dateForKey:(id)key withFormat:(NSString *)formatString{
+    NSString *string = [self JL_stringForKey:key];
+    if (!string) return nil;
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:formatString];
+    NSDate *date = [dateFormatter dateFromString:string];
+    return date;
 }
 
 @end
